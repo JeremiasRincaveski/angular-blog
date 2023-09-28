b69eca4b8fd6cd6c116bab6dcefce1cea1a4cbee
jeremiasRincaveski <jrincaveski@gmail.com>
2023-09-28 06:41:21 -0300

    noticias prontas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
-import { BigCardComponent } from './components/big-card/big-card.component';
-import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MainNewsComponent } from './components/main-news/main-news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     MenuTitleComponent,
-    BigCardComponent,
-    SmallCardComponent,
-    HomeComponent
+    HomeComponent,
+    MainNewsComponent
   ],
   imports: [
     BrowserModule,
